a5734d785844dd7571afa76c33062161ff03025e
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-08-12 15:09:04 -0400

    added links to api

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = ['WKOTzQ1n5pF9UNisttH54ctAvGGY3IfRFeK5P7Xn',
+                            'Y3kO2jfVMWEx8krgTcm8gFb9SaWAh2cI2dGwn6wu',
+                            'cFDS0T7ZdorZ7rAVZTa3YunnWnSFvOkMwo5dGYNv',
+                            '45etKnAlM1RBXG7xMzWHacJ72LANQLLOUt5T6Fa7',
+                            'fNaKMQv0wHq6QhIlmrdtSlECtErhlDkCduEjHB6L',
+                            'XTaOFFqiLyjt2qcA4thu6FEGsBw8PQaPLNNV7XHx',
+                            'UKeS1Y4G2eHHZ1rH0ctJdG8BHBUWeDM30k3UrKrL']
